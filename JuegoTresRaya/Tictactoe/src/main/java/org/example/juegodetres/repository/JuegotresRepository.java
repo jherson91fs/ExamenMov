b26d8def7a91f5367d97a0828a2a63a898db6aec
jherson91fs <jherson.fernandez.19@gmail.com>
2024-09-26 22:20:07 -0500

    hdgf

--- JuegoTresRaya/Tictactoe/src/main/java/org/example/juegodetres/repository/JuegotresRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.juegodetres.repository;
-
-import org.example.juegodetres.entity.Juegotres;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JuegotresRepository extends JpaRepository<Juegotres, Long> {
-    // Aquí puedes definir consultas personalizadas si es necesario
-}
